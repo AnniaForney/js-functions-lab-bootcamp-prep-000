c1421afa691c829b6ff33cb1fdfc622655704c2c
Annia Forney <anniaforney@gmail.com>
2019-03-11 19:27:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo (name) {
 }
 
 function happyHolidayTo () {
-  return ('Happy ${holiday}, ${name}!')
+  return (`Happy ${holiday}, ${name}!`)
   //console.log('Happy ${thanksgiving}, ${Camden}!')
 }
 
